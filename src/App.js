0ece1decd672304049d9a2f4b21d8f68d62e2229
Dan Soutar <dan.soutar@littlerocket.io>
2020-12-08 22:04:39 -0500

    finished tours project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = 'https://course-api.com/react-tours-project';
 function App() {
-  const [loading, setloading] = useState;
-  const [tours, setTours] = useState;
+  const [loading, setLoading] = useState(false);
+  const [tours, setTours] = useState([]);
   
+  
+  // Define Effect - fetch data
+  const fetchTours = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(url)
+      const tours = await response.json()
+      setLoading(false)
+      setTours(tours)
+      console.log(tours)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+
+  // Define Effect - Delete a tour 
+  const removeTour = (id) => {
+    const newTours = tours.filter( (tour)=> { return tour.id !== id} )
+    setTours(newTours)
+  }
+
+
+  // useEffect - fetch tours
+  useEffect(() => {
+    fetchTours()
+  }, []);
+
+
+  // Rendering
   if (loading) {
     return (
       <main>
@@ -15,8 +45,24 @@ function App() {
       </main>
     )
   }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No Tours Left</h2>
+          <button onClick={fetchTours}>Refresh</button>
+        </div>
+      </main>
+    )
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  )
   
-  return <h2>Tours Project Setup</h2>
 }
 
 export default App
